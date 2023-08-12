77da6a995e2739694ef850119ab6b8d16f56ac17
kadirtuna <kadirtunaceeng@gmail.com>
2023-08-12 22:53:03 +0300

    An update has been added!

--- practical-works/piano-website/script.js
+++ practical-works/piano-website/script.js
@@ -165,7 +165,7 @@ for (let i = 0; i < 88; i++){
     }
 
     let html = `
-    <a href="#piano__header_scrollbar" id="note${i}">
+    <a href="#piano__main_keys" id="note${i}">
         <div class="${str}">
             <h3 style="display: ${display};background-color: ${colorsRegardingRange[counter]}" data-key="${dataKey}">${text}${counterTemp}</h3>
         </div>
